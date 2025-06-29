827013b743aa896217ac54ef9057c82fc4a787a4
- <perkinsokunek@gmail.com>
2025-06-29 13:31:46 +0300

    Add project files.

---
+++ VendingMachine/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VendingMachine
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
